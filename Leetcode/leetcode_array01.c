16b87350d889c996afc0d3389a3b36963ca164c1
dinooh <hooneec1117@gmail.com>
2020-12-11 14:57:08 +0900

    2020/12/11 (THU) 14:57

--- Leetcode/leetcode_array01.c
+++ Leetcode/leetcode_array01.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   leetcode_array01.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jeunjeon <jeunjeon@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/11 10:43:06 by jeunjeon          #+#    #+#             */
-/*   Updated: 2020/12/11 10:43:21 by jeunjeon         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 
 int findMaxConsecutiveOnes(int* nums, int numsSize)
@@ -28,11 +16,4 @@ int findMaxConsecutiveOnes(int* nums, int numsSize)
         temp = 0;
     }
     return (len);
-}
-
-int main(void)
-{
-	int	arr[] = {1, 0, 1, 1, 0, 1};
-	printf("%d\n", findMaxConsecutiveOnes(arr, 1));
-	return (0);
 }
